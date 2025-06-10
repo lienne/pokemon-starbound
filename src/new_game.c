61eb3a578f45527308b1e887faffd1d46331dd8c
Belle <lianasevero3@gmail.com>
2025-06-10 01:15:39 -0400

    Map fixes

--- src/new_game.c
+++ src/new_game.c
@@ -132,7 +132,7 @@ static void ClearFrontierRecord(void)
 static void WarpToTruck(void)
 {
     // SetWarpDestination(MAP_GROUP(MAP_INSIDE_OF_TRUCK), MAP_NUM(MAP_INSIDE_OF_TRUCK), WARP_ID_NONE, -1, -1);
-    SetWarpDestination(MAP_GROUP(MAP_LUMEN_VILLAGE), MAP_NUM(MAP_LUMEN_VILLAGE), WARP_ID_NONE, 26, 22);
+    SetWarpDestination(MAP_GROUP(MAP_LUMEN_VILLAGE), MAP_NUM(MAP_LUMEN_VILLAGE), WARP_ID_NONE, 36, 22);
     WarpIntoMap();
 }
 
